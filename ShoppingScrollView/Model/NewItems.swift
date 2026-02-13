bc655faad2c4ebcb3b1a12446f0639f6dfaf638f
yash <yashrajsinhjadeja2253@gmail.com>
2026-02-13 16:30:30 +0530

    New Item partial done

---
+++ ShoppingScrollView/Model/NewItems.swift
@@ -0,0 +1,8 @@
+import Foundation
+import UIKit
+
+struct NewItems {
+    let imageName: String
+    let title: String
+    let price: String
+}
---
+++ ShoppingScrollView/View/NewItemsCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  NewItemsCollectionViewCell.swift
+//  ShoppingScrollView
+//
+//  Created by Yashraj on 13/02/26.
+//
+
+import UIKit
+
+class NewItemsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imgNewItems: UIImageView!
+    @IBOutlet weak var txtTitle: UILabel!
+    @IBOutlet weak var txtPrice: UILabel!
+}
